115e143b3421e74d676910792939459ccd59ee96
aischeveva <aslonenok@gmail.com>
2017-01-20 12:21:18 +0300

    Homework 8, var 1
    и другие файлы

---
+++ homework_prog/homework_8/hw8.py
@@ -0,0 +1,53 @@
+import random
+
+
+def ask_name():
+    return input('Введите имя файла с расширением: ')
+
+
+def get_words():
+    f = open(ask_name(), 'r', encoding='UTF-8')
+    s = f.read().split('\n')
+    f.close()
+    dic = {}
+    for ln in s:
+        temp = ln.split(',')
+        dic[temp[0]] = temp[1:]
+    return dic
+
+
+def guess_word(word):
+    num = len(word)
+    if num <= 4:
+        print('У вас {0} попытки'.format(num))
+    else:
+        print('У вас {0} попыток'.format(num))
+    while num > 0:
+        temp = input('Введите слово: ')
+        if temp == word:
+            print('Вы угадали!')
+            break
+        else:
+            print('Попробуйте еще раз!')
+            num -= 1
+    if num == 0:
+        print('Повезет в другой раз!')
+
+
+def game(d):
+    ask = ''
+    for k in d:
+        print('Подсказка! {0} ...'.format(random.choice(d[k])))
+        guess_word(k)
+        ask = input('Хотите попробовать еще раз? Введите только "да" или "нет": ')
+        if ask == 'нет':
+            break
+    if ask == 'да':
+        print('Жаль, только слова уже кончились. Приходите еще')
+
+def main():
+    d = get_words()
+    game(d)
+
+
+main()
